ef18e81ea9a31c7c4f0c798118648ddf1905a3b5
Nolan Jensen <53351090+NolJen@users.noreply.github.com>
2024-10-16 13:22:00 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//step 1 create button and emoji
+const button = document.createElement("button");
+button.innerHTML = "👾"
+button.type = "button"
+
+app.append(button);
+//refrenced https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
