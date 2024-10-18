c86f126cb06c9b36d2cc8fb9c4ebb380e748ac51
Nolan Jensen <53351090+NolJen@users.noreply.github.com>
2024-10-18 11:47:35 -0700

    Step 2
    redoing each step because I did pages wrong

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,24 @@ app.append(header);
 
 //step 1 create button and emoji
 const button = document.createElement("button");
-button.innerHTML = "👾";
+button.innerHTML = "👾 click me";
 button.type = "button";
+button.name = "funButton";
+button.title = "Click me for fun!";
 
 app.append(button);
 //refrenced https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
+
+// Create a div to report on the value of the counter
+const counterDiv = document.createElement("div");
+let counter: number = 0; //You should probably have some like like “let counter: number = 0;” in your program somewhere.
+counterDiv.innerHTML = `${counter} purple people eaters;`
+app.append(counterDiv);
+
+// Add an event listener to the button to increment the counter
+button.addEventListener("click", () => {
+  counter++;
+  counterDiv.innerHTML = `${counter} purple people eaters;`
+});
+// refrenced https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+// and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
